e097c0b05670730ff85e87206b8181bf44b63f13
Administrator <1840383012@qq.com>
2019-06-06 18:19:11 +0800

    直接选择排序

--- app/src/main/java/net/lzzy/algorithm/MainActivity.java
+++ app/src/main/java/net/lzzy/algorithm/MainActivity.java
@@ -54,7 +54,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void directSort() {
         //todo:直接选择排序的具体实现
-    }
+        int num=10;
+        for(int i=0;i<=num-1;i++){
+           for (int j=0;j<num- i;j++){
+               if(items[j]>items[j+1]){
+                   int temp;
+                   temp=items[j];
+                   items[j]=items[j+1];
+                   items[j+1]=temp;
+               }
+           }
+            }
+        }
+        //i指向无序区的开始位置，j对无序区进行扫描，把j扫描到的最小值记录到k去。
+        //然后k记录下无序区最小的数字,k记录下的最小数字与i进行交换完成排序，然后i前面的数字就是排列好的。
+        //i当前的位置里的数字如果大于无序区里的数字，则k指向无序区里最小的数字，然后k与i进行交换，在i前面完成排序
+
 
     private void generateItems() {
         items = new Integer[10];
